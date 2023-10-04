af3b269f1a78dbc049ae7b03f28d968276913734
Yash Vijay Bombale <yashbombale@gmail.com>
2023-10-04 22:35:36 +0530

    commit 1:Created an home-page ,status:done

---
+++ src/components/Header.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+  useDisclosure,
+  VStack,
+  HStack,
+} from '@chakra-ui/react';
+
+import { Link } from 'react-router-dom';
+import { BiMenuAltLeft } from 'react-icons/bi';
+const Header = () => {
+  // !where ever there is use keyword infront of any functions they are known as 'Hooks';
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button
+        pos={'fixed'}
+        left={'4'}
+        top={'4'}
+        colorScheme="purple"
+        p={'0'}
+        w={'10'}
+        h={'10'}
+        borderRadius={'full'}
+        onClick={onOpen}
+      >
+        <BiMenuAltLeft size={'20'} />
+      </Button>
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>YOUTUBE</DrawerHeader>
+          <DrawerBody>
+            {/*Vstack is for which is having display:flex and flex-direction as row */}
+            <VStack alignItems={'flex-start'}>
+              <Button onClick={onClose} variant={'ghost'} colorScheme="purple">
+                <Link to={'/'}>Home</Link>
+              </Button>
+              <Button onClick={onClose} variant={'ghost'} colorScheme="purple">
+                <Link to={'/videos'}>Videos</Link>
+              </Button>
+              <Button onClick={onClose} variant={'ghost'} colorScheme="purple">
+                <Link to={'/videos?categoru=free'}>Free videos</Link>
+              </Button>
+              <Button onClick={onClose} variant={'ghost'} colorScheme="purple">
+                <Link to={'/upload'}>Upload video</Link>
+              </Button>
+            </VStack>
+            <HStack
+              pos={'absolute'}
+              bottom={'10'}
+              left={'0'}
+              w={'full'}
+              justifyContent={'space-evenly'}
+            >
+              <Button onClick={onClose} colorScheme="purple">
+                <Link to={'/login'}>Log In</Link>
+              </Button>
+              <Button
+                onClick={onClose}
+                variant={'outline'}
+                colorScheme="purple"
+              >
+                <Link to={'/signup'}>Sign Up</Link>
+              </Button>
+            </HStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/components/Home.jsx
@@ -0,0 +1,104 @@
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
+
+import { Carousel } from 'react-responsive-carousel';
+
+//  * Importing an css file for carousel;
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+// *Importing an images from assests;
+
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+import img4 from '../assets/4.jpg';
+import img5 from '../assets/5.png';
+
+const headingOptions = {
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%, -50%)',
+  p: '4',
+  size: '3xl',
+};
+const Home = () => {
+  return (
+    <Box>
+      <MyCarousel />
+      <Container minH={'100vh'} maxW={'container.xl'} p={'16'}>
+        <Heading
+          textTransform={'uppercase'}
+          py={'2'}
+          borderBottom={'2px solid'}
+          w={'fit-content'}
+          m={'auto'}
+        >
+          Services
+        </Heading>
+        <Stack
+          h={'full'}
+          py={'4'}
+          alignItems={'center'}
+          direction={['column', 'row']}
+        >
+          <Image src={img5} h={['40', '400']} filter={'hue-rotate(-130deg)'} />
+          <Text
+            letterSpacing={'widest'}
+            lineHeight={'190%'}
+            p={[4, 16]}
+            textAlign={'center'}
+          >
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quasi
+            aliquam, suscipit ad saepe culpa numquam modi odio dolor
+            exercitationem labore illo ipsum at ullam enim quo reprehenderit
+            eligendi in iste facilis tempora veniam adipisci dolorum officiis!
+            Eos quae, esse necessitatibus assumenda saepe quaerat nemo
+            accusantium maiores at? Hic, aspernatur aliquid labore quidem omnis
+            natus modi incidunt voluptatem beatae nostrum ut! Quidem dicta nemo
+            corrupti odit culpa. Voluptates reprehenderit tenetur est dolore
+            blanditiis
+          </Text>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+const MyCarousel = () => (
+  <Carousel
+    autoPlay
+    infiniteLoop
+    interval={2000}
+    showStatus={false}
+    showThumbs={false}
+    showArrows={false}
+  >
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img1} />
+      <Heading bgColor={'blackAlpha.400'} color={'white'} {...headingOptions}>
+        Watch The Future
+      </Heading>
+    </Box>
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img2} />
+      <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions}>
+        Gaming Is Future
+      </Heading>
+    </Box>
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img3} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Gaming On Console
+      </Heading>
+    </Box>
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img4} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        night life is cool
+      </Heading>
+    </Box>
+  </Carousel>
+);
+
+export default Home;
